0304a2835ff80f50ac51561a6b04bd250da2cb98
wingsummer <1326224942@qq.com>
2022-07-30 23:46:24 +0800

    update translation

--- main.cpp
+++ main.cpp
@@ -1,14 +1,43 @@
 #include "Dialog/mainwindow.h"
 #include <DApplication>
 #include <DApplicationSettings>
+#include <DMessageBox>
 #include <DTitlebar>
 #include <DWidgetUtil>
+#include <QTranslator>
 
 DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[]) {
+  //解决 root/ubuntu 主题样式走形
+  qputenv("XDG_CURRENT_DESKTOP", "Deepin");
   QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-  DApplication a(argc, argv);
+
+  // 程序内强制添加 -platformtheme
+  // deepin 参数喂给 Qt 让 Qt 正确使用 Deepin 主题修复各种奇怪样式问题
+  QVector<char *> fakeArgs(argc + 2);
+  char fa1[] = "-platformtheme";
+  char fa2[] = "deepin";
+  fakeArgs[0] = argv[0];
+  fakeArgs[1] = fa1;
+  fakeArgs[2] = fa2;
+
+  for (int i = 1; i < argc; i++)
+    fakeArgs[i + 2] = argv[i];
+  int fakeArgc = argc + 2;
+  DApplication a(fakeArgc, fakeArgs.data());
+  QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
+
+  auto s = a.applicationDirPath() + "/lang/default.qm";
+  QTranslator translator;
+  if (!translator.load(s)) {
+    DMessageBox::critical(nullptr, "Error", "Error Loading Translation File!",
+                          DMessageBox::Ok);
+    return -1;
+  }
+
+  a.installTranslator(&translator);
+
   a.setOrganizationName("WingCloud");
   a.setApplicationName("WingGifEditor");
   a.setApplicationVersion("1.0");
